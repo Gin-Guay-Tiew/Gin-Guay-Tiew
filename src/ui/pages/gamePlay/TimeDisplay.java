12a1d6613e0f20acda429c3ffb6e86e7c80cf768
Napapha Thoowasin <isnan.napapha@gmail.com>
2026-03-09 16:07:53 +0700

    Update TimeDisplay.java

--- src/ui/pages/gamePlay/TimeDisplay.java
+++ src/ui/pages/gamePlay/TimeDisplay.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 public class TimeDisplay extends JPanel {
     private final Font jerseyFont = FontLoader.loadCustomFont("resources/font/Jersey10.ttf");
+    private JLabel timeLabel;
 
     public TimeDisplay(){
 
@@ -14,9 +15,14 @@ public class TimeDisplay extends JPanel {
         setBorder(BorderFactory.createEmptyBorder(10, 15, 5, 15));
         setBackground(new Color(225, 225, 225));
 
-        JLabel timeLabel = new JLabel("Time : 1:30");
+        timeLabel = new JLabel("Time : 1:30");
         timeLabel.setFont(jerseyFont.deriveFont(25f));
-
         add(timeLabel, BorderLayout.CENTER);
+    }
+
+    public void updateTime(int totalSeconds){
+        int min = totalSeconds / 60;
+        int sec = totalSeconds % 60;
+        timeLabel.setText(String.format("Time : %02d:%02d", min, sec));
     }
 }
