9657c45d05a63be419abc99e5a51b5978a29adbe
Napapha Thoowasin <isnan.napapha@gmail.com>
2026-03-09 17:23:28 +0700

    Update TimeDisplay.java

--- src/ui/pages/gamePlay/TimeDisplay.java
+++ src/ui/pages/gamePlay/TimeDisplay.java
@@ -7,22 +7,44 @@ import java.awt.*;
 
 public class TimeDisplay extends JPanel {
     private final Font jerseyFont = FontLoader.loadCustomFont("resources/font/Jersey10.ttf");
-    private JLabel timeLabel;
+    private JLabel minLabel;
+    private JLabel secLabel;
 
     public TimeDisplay(){
 
-        setLayout(new BorderLayout());
-        setBorder(BorderFactory.createEmptyBorder(10, 15, 5, 15));
+        setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+        setBorder(BorderFactory.createEmptyBorder(15, 15, 0, 15));
         setBackground(new Color(225, 225, 225));
 
-        timeLabel = new JLabel("Time : 1:30");
-        timeLabel.setFont(jerseyFont.deriveFont(25f));
-        add(timeLabel, BorderLayout.CENTER);
+        Font font25 =  jerseyFont.deriveFont(30f);
+
+        JLabel timeLabel = new JLabel("Time : ");
+        timeLabel.setFont(font25);
+
+        minLabel = new JLabel("00");
+        minLabel.setFont(font25);
+        minLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        minLabel.setPreferredSize(new Dimension(25,25));
+
+        JLabel colonLabel = new JLabel(":");
+        colonLabel.setFont(font25);
+
+        secLabel = new JLabel("00");
+        secLabel.setFont(font25);
+        secLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        secLabel.setPreferredSize(new Dimension(25,25));
+
+        add(timeLabel);
+        add(minLabel);
+        add(colonLabel);
+        add(secLabel);
     }
 
     public void updateTime(int totalSeconds){
         int min = totalSeconds / 60;
         int sec = totalSeconds % 60;
-        timeLabel.setText(String.format("Time : %02d:%02d", min, sec));
+
+        minLabel.setText(String.format("%02d",min));
+        secLabel.setText(String.format("%02d",sec));
     }
 }
