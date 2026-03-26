06a3c90bf078d550a4cd52f4a4409d8963daff60
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-03-26 22:18:18 +0700

    edit size button in esc page

---
+++ src/ui/pages/pressESC/FadeOverlay.java
@@ -0,0 +1,4 @@
+package ui.pages.pressESC;
+
+public class FadeOverlay {
+}
--- src/ui/pages/pressESC/PauseScreen.java
+++ src/ui/pages/pressESC/PauseScreen.java
@@ -1,46 +1,90 @@
 package ui.pages.pressESC;
+
 import main.MainFrame;
 import ui.components.ImageJButton;
 import utilities.IconImage;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PauseScreen extends JPanel {
+
+    private Image backgroundImage;
+
     public PauseScreen(MainFrame mainFrame) {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setOpaque(false);
 
-        add(Box.createVerticalStrut(120));
+        setLayout(new GridBagLayout());
 
-        JLabel logo = new JLabel(
-                IconImage.create("resources/images/mainMenu/LogoGame.png", 300, 120)
+        // ================= Background =================
+        ImageIcon original = new ImageIcon("resources/images/mainMenu/Background.gif");
+        backgroundImage = original.getImage();
+
+        // ================= Center Container =================
+        JPanel centerContain = new JPanel();
+        centerContain.setLayout(new BoxLayout(centerContain, BoxLayout.Y_AXIS));
+        centerContain.setOpaque(false);
+
+        // ================= Logo =================
+        ImageIcon icon = IconImage.create(
+                "resources/images/mainMenu/LogoGame.png",
+                280, 280
         );
-        logo.setAlignmentX(CENTER_ALIGNMENT);
 
-        add(logo);
-        add(Box.createVerticalStrut(40));
+        JLabel logo = new JLabel(icon);
+        logo.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        centerContain.add(logo);
+        centerContain.add(Box.createVerticalStrut(20));
+
+        // ================= Buttons =================
         JButton resume = new ImageJButton(
-                "resources/images/endGame/BackToTheGame", ".png", 30, 280, 70
+                "resources/images/endGame/BackToTheGame", ".png", 30, 250, 40
         );
 
         JButton setting = new ImageJButton(
-                "resources/images/mainMenu/buttons/Settings", ".png", 30, 280, 70
+                "resources/images/mainMenu/buttons/Settings", ".png", 30, 250, 40
         );
 
         JButton menu = new ImageJButton(
-                "resources/images/endGame/backToMenu", ".png", 30, 280, 70
+                "resources/images/endGame/backToMenu", ".png", 30, 250, 40
         );
 
-        resume.setAlignmentX(CENTER_ALIGNMENT);
-        setting.setAlignmentX(CENTER_ALIGNMENT);
-        menu.setAlignmentX(CENTER_ALIGNMENT);
+        resume.setAlignmentX(Component.CENTER_ALIGNMENT);
+        setting.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menu.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        add(resume);
-        add(Box.createVerticalStrut(20));
-        add(setting);
-        add(Box.createVerticalStrut(20));
-        add(menu);
+        centerContain.add(resume);
+        centerContain.add(Box.createVerticalStrut(10));
+        centerContain.add(setting);
+        centerContain.add(Box.createVerticalStrut(10));
+        centerContain.add(menu);
 
+        // ================= Center Layout =================
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        add(centerContain, gbc);
+
+        // ================= Button Action =================
+
+        resume.addActionListener(e ->
+                mainFrame.getNavigator().toPage(mainFrame.getPreviousPage(), false)
+        );
+
+        setting.addActionListener(e ->
+                mainFrame.getNavigator().toPage(MainFrame.SETTING, true)
+        );
+
+        menu.addActionListener(e ->
+                mainFrame.getNavigator().toPage(MainFrame.MAIN_MENU, true)
+        );
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
